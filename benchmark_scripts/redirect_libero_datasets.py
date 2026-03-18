9bc4e4dc57099149d6a6da3a3e810d6a6498c93c
hkx2024 <121613993+hkx2024@users.noreply.github.com>
2026-03-18 16:05:22 +0800

    Create redirect_libero_datasets.py

---
+++ benchmark_scripts/redirect_libero_datasets.py
@@ -0,0 +1,13 @@
+import init_path
+import argparse
+import os
+import time
+
+from libero.libero import get_libero_path
+
+def main():
+  print("重定向libero数据集！")
+
+
+if __name__ == "__main__":
+    main()
